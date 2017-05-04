4033e817f2af128da1d3ee2b6768d5a124ed1291
TekatoD <ocnechlahim@mail.ru>
2017-05-04 18:36:05 +0300

    Some another fix

--- src/Pose2D.cpp
+++ src/Pose2D.cpp
@@ -65,7 +65,7 @@ Robot::Pose2D& Robot::Pose2D::operator-=(const Robot::Pose2D &rhs) {
 }
 
 void Robot::Pose2D::normalizeThetaInPi() {
-    m_theta = remainder(m_theta, M_PI);
+    m_theta = atan2(sin(m_theta), cos(m_theta));
 }
 
 namespace Robot {

e292c6e0725e499680071ff480c1adebccfa6385
Artem Kovalev <leon112rus@gmail.com>
2017-11-16 16:17:19 +0300

    Add localization_t skeleton

---
+++ include/localization/localization_t.h
@@ -0,0 +1,53 @@
+//
+// Created by akovalev on 16.11.17.
+//
+
+#pragma once
+
+#include <localization/localization_util_t.h>
+#include <localization/particle_filter_t.h>
+#include <vision/vision_utils.h>
+
+namespace drwn {
+    class localization_t {
+    public:
+        static localization_t* get_instance();
+
+        // Motion data
+        void set_pose_shift(pose_2D_t pose_shift);
+        // Sensor data
+        void set_lines(std::vector<cv::Vec4i> lines);
+
+        void update();
+
+        // Resets PF particles to current pose
+        void set_current_pose(pose_2D_t current_pose);
+
+        void set_field(field_map_t field);
+        field_map_t get_field() const;
+
+        void set_camera_parameters(vision_utils::camera_parameters_t camera_params);
+        vision_utils::camera_parameters_t get_camera_parameters();
+
+        // Resets PF particles to area
+        void set_pose_approximate_area(pose_2D_t min_pose, pose_2D_t max_pose);
+
+        // Resets PF to field area
+        void set_pose_approximate_area_to_field();
+
+        void set_particle_filter(particle_filter_t* pf_ptr);
+        particle_filter_t* get_particle_filter() const;
+
+        pose_2D_t get_calculated_pose_mean();
+        pose_2D_t get_calculated_pose_std_dev();
+        //
+    private:
+        localization_t();
+
+        particle_filter_t* m_particle_filter;
+
+        // TODO: camera_params move to hw
+        vision_utils::camera_parameters_t m_camera_params;
+        vision_utils::camera_projection_t m_camera_projection;
+    };
+}
---
+++ src/localization/localization_t.cpp
@@ -0,0 +1,95 @@
+//
+// Created by akovalev on 16.11.17.
+//
+
+#include <localization/localization_t.h>
+
+using namespace drwn;
+
+localization_t* localization_t::get_instance() {
+    static localization_t instance;
+    return &instance;
+}
+
+localization_t::localization_t()
+{
+
+}
+
+void localization_t::set_pose_shift(pose_2D_t pose_shift)
+{
+
+}
+
+void localization_t::set_lines(std::vector<cv::Vec4i> lines)
+{
+
+}
+
+void localization_t::update()
+{
+
+}
+
+void localization_t::set_current_pose(pose_2D_t current_pose)
+{
+    assert(m_particle_filter != nullptr);
+}
+
+void localization_t::set_field(field_map_t field)
+{
+    assert(m_particle_filter != nullptr);
+    m_particle_filter->set_field_world(field);
+}
+
+field_map_t localization_t::get_field() const
+{
+    assert(m_particle_filter != nullptr);
+    return *(m_particle_filter->get_field_world());
+}
+
+void localization_t::set_camera_parameters(vision_utils::camera_parameters_t camera_params)
+{
+
+}
+
+vision_utils::camera_parameters_t localization_t::get_camera_parameters()
+{
+    return m_camera_params;
+}
+
+void localization_t::set_pose_approximate_area(pose_2D_t min_pose, pose_2D_t max_pose)
+{
+
+}
+
+void localization_t::set_pose_approximate_area_to_field()
+{
+
+}
+
+void localization_t::set_particle_filter(particle_filter_t* pf_ptr)
+{
+    assert(pf_ptr != nullptr);
+    m_particle_filter = pf_ptr;
+}
+
+particle_filter_t* localization_t::get_particle_filter() const
+{
+    assert(m_particle_filter != nullptr);
+    return m_particle_filter;
+}
+
+pose_2D_t localization_t::get_calculated_pose_mean()
+{
+    assert(m_particle_filter != nullptr);
+    return m_particle_filter->get_pose_mean();
+}
+
+pose_2D_t localization_t::get_calculated_pose_std_dev()
+{
+    assert(m_particle_filter != nullptr);
+    return m_particle_filter->get_pose_std_dev();
+}
+
+

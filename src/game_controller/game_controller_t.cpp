c9dea72f5672d1008c90d737f7876d779c8dec8a
Arseniy Ivin <arssivka@yandex.ru>
2017-11-16 16:41:54 +0300

    Game controller fixes. More debug output for game controller

--- src/game_controller/game_controller_t.cpp
+++ src/game_controller/game_controller_t.cpp
@@ -10,14 +10,14 @@
 using namespace drwn;
 
 game_controller_t* game_controller_t::get_instance() {
-    static game_controller_t gameController;
-    return &gameController;
+    static game_controller_t instance;
+    return &instance;
 }
 
 
 void game_controller_t::reset() {
     memset(&m_game_controller_address, 0, sizeof(m_game_controller_address));
-    memset(&game_ctrl_data, 0, sizeof(game_ctrl_data));
+    memset(&m_game_ctrl_data, 0, sizeof(m_game_ctrl_data));
 
     m_when_packet_was_received = 0;
     m_when_packet_was_sent = 0;
@@ -27,18 +27,18 @@ void game_controller_t::reset() {
 void game_controller_t::update() {
     clock_t now = clock();
 
-    if (receive()) {
+    if (this->receive()) {
         m_when_packet_was_received = now;
     }
 
-    if(now - m_when_packet_was_received < GAMECONTROLLER_TIMEOUT &&
-       now - m_when_packet_was_sent >= ALIVE_DELAY) {
-        send_alive();
+    if (now - m_when_packet_was_received < GAMECONTROLLER_TIMEOUT &&
+        now - m_when_packet_was_sent >= ALIVE_DELAY) {
+        this->send_alive();
     }
 }
 
 
-bool game_controller_t::game_controller_not_responding() const {
+bool game_controller_t::not_responding() const {
     clock_t now = clock();
     return now - m_when_packet_was_received < GAMECONTROLLER_TIMEOUT;
 }
@@ -48,18 +48,22 @@ bool game_controller_t::connect() {
     if (m_udp) return true;
     m_udp = new udp_comm_t();
     if (m_udp->set_blocking(false) &&
-            m_udp->set_broadcast(true) &&
+        m_udp->set_broadcast(true) &&
         m_udp->bind("0.0.0.0", GAMECONTROLLER_DATA_PORT) &&
-            m_udp->set_loopback(false)) {
+        m_udp->set_loopback(false)) {
+        if (m_debug) LOG_DEBUG << "GAME CONTROLLER: Server socket has been bound";
         return true;
+    } else {
+        if (m_debug) LOG_DEBUG << "GAME CONTROLLER: Can't bind server socket";
+        delete m_udp;
+        return false;
     }
-    delete m_udp;
-    return false;
 }
 
 
 void game_controller_t::disconnect() {
-    if (m_udp) delete m_udp;
+    if (m_debug) LOG_DEBUG << "GAME CONTROLLER: Game controller disconnected";
+    delete m_udp;
 }
 
 
@@ -69,67 +73,70 @@ bool game_controller_t::connected() const {
 
 
 void game_controller_t::send_penalise() {
-    if (send(GAMECONTROLLER_RETURN_MSG_MAN_PENALISE)) {
+    if (this->send(GAMECONTROLLER_RETURN_MSG_MAN_PENALISE)) {
+        if (m_debug) LOG_INFO << "GAME CONTROLLER: Sending manual penalise message";
         m_when_packet_was_sent = clock();
     }
 }
 
 
 void game_controller_t::send_unpenalise() {
-    if (send(GAMECONTROLLER_RETURN_MSG_MAN_UNPENALISE)) {
+    if (this->send(GAMECONTROLLER_RETURN_MSG_MAN_UNPENALISE)) {
+        if (m_debug) LOG_INFO << "GAME CONTROLLER: Sending manual unpenalise message";
         m_when_packet_was_sent = clock();
     }
 }
 
 
 void game_controller_t::send_alive() {
-    if (send(GAMECONTROLLER_RETURN_MSG_ALIVE)) {
+    if (this->send(GAMECONTROLLER_RETURN_MSG_ALIVE)) {
+        if (m_debug) LOG_INFO << "GAME CONTROLLER: Sending alive message";
         m_when_packet_was_sent = clock();
     }
 }
 
 
 int game_controller_t::get_player_number() const {
-    return player_number;
+    return m_player_number;
 }
 
 void game_controller_t::set_player_number(int player_number) {
-    if(m_debug) {
+    if (m_debug) {
         LOG_DEBUG << "GAME CONTROLLER: player_number = " << player_number;
     }
-    player_number = player_number;
+    m_player_number = player_number;
 }
 
 int game_controller_t::get_team_number() const {
-    return team_number;
+    return m_team_number;
 }
 
 void game_controller_t::set_team_number(int team_number) {
-    if(m_debug) {
+    if (m_debug) {
         LOG_DEBUG << "GAME CONTROLLER: team_number = " << team_number;
     }
-    team_number = team_number;
+    m_team_number = team_number;
 }
 
 
 game_controller_t::~game_controller_t() {
-    disconnect();
+    this->disconnect();
 }
 
 
 game_controller_t::game_controller_t(int player_num, int team_num)
-        : player_number(player_num),
-          team_number(team_num) {
-    reset();
+        : m_player_number(player_num),
+          m_team_number(team_num) {
+    this->reset();
 }
 
 
 bool game_controller_t::send(uint8_t message) {
-    robo_cup_game_control_return_data_t returnPacket;
-    returnPacket.team = (uint8_t) team_number;
-    returnPacket.player = (uint8_t) player_number;
-    returnPacket.message = message;
-    return !m_udp || m_udp->write((const char*) &returnPacket, sizeof(returnPacket));
+    robo_cup_game_control_return_data_t return_packet;
+    return_packet.team = (uint8_t) m_team_number;
+    return_packet.player = (uint8_t) m_player_number;
+    return_packet.message = message;
+    return !m_udp || m_udp->write((const char*) &return_packet, sizeof(return_packet));
 }
 
 
@@ -142,10 +149,11 @@ bool game_controller_t::receive() {
         if (size == sizeof(buffer) &&
             !std::memcmp(&buffer, GAMECONTROLLER_STRUCT_HEADER, 4) &&
             buffer.version == GAMECONTROLLER_STRUCT_VERSION &&
-            team_number &&
-            (buffer.teams[0].team_number == team_number ||
-             buffer.teams[1].team_number == team_number)) {
-            game_ctrl_data = buffer;
+            m_team_number &&
+            (buffer.teams[0].team_number == m_team_number ||
+             buffer.teams[1].team_number == m_team_number)) {
+            if (m_debug) LOG_DEBUG << "GAME CONTROLLER: Message received";
+            m_game_ctrl_data = buffer;
             if (memcmp(&m_game_controller_address, &from.sin_addr, sizeof(in_addr))) {
                 memcpy(&m_game_controller_address, &from.sin_addr, sizeof(in_addr));
                 m_udp->set_target(inet_ntoa(m_game_controller_address), GAMECONTROLLER_RETURN_PORT);
@@ -161,5 +169,5 @@ bool game_controller_t::is_debug_enabled() const {
 }
 
 void game_controller_t::enable_debug(bool debug) {
-    m_debug= debug;
+    m_debug = debug;
 }

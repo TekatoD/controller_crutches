f362864fcfc4b5abcd30a7bd874332016ae64b21
Artem Kovalev <leon112rus@gmail.com>
2017-11-21 17:36:12 +0300

    Calculate leg forward kinematics for correct head height

--- src/localization/localization_t.cpp
+++ src/localization/localization_t.cpp
@@ -7,6 +7,7 @@
 // TODO: getter setter for head
 #include <motion/modules/head_t.h>
 #include <motion/kinematics_t.h>
+#include <motion/motion_status_t.h>
 
 using namespace drwn;
 
@@ -46,17 +47,39 @@ void localization_t::update()
     float head_pan = head->get_pan_angle() * (M_PI / 180.0f);
     float head_tilt = head->get_tilt_angle() * (M_PI / 180.0f);
 
+    float ankle_pitch = motion_status_t::current_joints.get_radian(joint_data_t::ID_L_ANKLE_PITCH);
+    float ankle_roll = motion_status_t::current_joints.get_radian(joint_data_t::ID_L_ANKLE_ROLL);
+    float knee_pitch = motion_status_t::current_joints.get_radian(joint_data_t::ID_L_KNEE);
+    // TODO: Check correctness
+    float thigh_pitch = motion_status_t::current_joints.get_radian(joint_data_t::ID_L_HIP_PITCH);
+    float thigh_roll = motion_status_t::current_joints.get_radian(joint_data_t::ID_L_HIP_ROLL);
+    float pelvis = motion_status_t::current_joints.get_radian(joint_data_t::ID_L_HIP_YAW);
+
+    // HEAD TRANSFORM
     matrix4x4f_t head_transform_eigen;
     cv::Mat head_transform;
 
     kinematics_t::compute_head_forward_kinematics(head_transform_eigen, head_pan, head_tilt);
     cv::eigen2cv(head_transform_eigen, head_transform);
 
-    cv::Mat R = head_transform(cv::Range(0, 3), cv::Range(0, 3));
+    // LEFT LEG TRANSFORM
+    matrix4x4f_t left_leg_transform_eigen;
+    cv::Mat left_leg_transform;
+
+    kinematics_t::compute_leg_forward_kinematics(left_leg_transform_eigen, pelvis, thigh_roll, thigh_pitch,
+                                                 knee_pitch, ankle_pitch, ankle_roll);
+    cv::eigen2cv(left_leg_transform_eigen, left_leg_transform);
+
+    // Extract head rotation matrix and translation vector for projecting lines
+    cv::Mat head_rotation_matrix = head_transform(cv::Range(0, 3), cv::Range(0, 3));
     // Translation is in mm
-    cv::Mat t = head_transform(cv::Range(0, 3), cv::Range(3, 4));
-    //t /= 1000.0f;
-    t.at<float>(2, 0) += height_from_ground;
+    cv::Mat head_translation_vector = head_transform(cv::Range(0, 3), cv::Range(3, 4));
+    //head_translation_vector.at<float>(2, 0) += height_from_ground;
+
+    // Extract leg translation vector for calculating correct height from ground to head
+    cv::Mat leg_translation_vector = left_leg_transform(cv::Range(0, 3), cv::Range(3, 4));
+    leg_translation_vector.at<float>(2, 0) += 122.2f + 50.5f + kinematics_t::CAMERA_OFFSET_Z;
+    head_translation_vector += leg_translation_vector;
 
     // Collect measurements from lines
     particle_filter_t::measurement_bundle rangeBearingData;
@@ -71,8 +94,12 @@ void localization_t::update()
         mp1 = (cv::Mat_<float>(3, 1) << line[0], line[1], 1);
         mp2 = (cv::Mat_<float>(3, 1) << line[2], line[3], 1);
 
-        gp1 = m_camera_projection.image_to_world_explicit(mp1, R, t, m_camera_params.width, m_camera_params.height, m_camera_params.fov);
-        gp2 = m_camera_projection.image_to_world_explicit(mp2, R, t, m_camera_params.width, m_camera_params.height, m_camera_params.fov);
+        gp1 = m_camera_projection.image_to_world_explicit(mp1, head_rotation_matrix, head_translation_vector,
+                                                          m_camera_params.width, m_camera_params.height,
+                                                          m_camera_params.fov);
+        gp2 = m_camera_projection.image_to_world_explicit(mp2, head_rotation_matrix, head_translation_vector,
+                                                          m_camera_params.width, m_camera_params.height,
+                                                          m_camera_params.fov);
         float x1 = gp1.at<float>(0, 0);
         // change direction of get_y axis
         float y1 = -1*gp1.at<float>(1, 0);

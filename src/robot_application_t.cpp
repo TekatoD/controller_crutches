777c6692f9051b89cd427c28503992639d78fe8a
Arseniy Ivin <arssivka@yandex.ru>
2017-11-21 14:22:02 +0300

    Changes!

--- src/robot_application_t.cpp
+++ src/robot_application_t.cpp
@@ -228,9 +228,6 @@ void robot_application_t::init_motion_modules() {
     motion_manager_t::get_instance()->add_module((motion_module_t*) walking_t::get_instance());
     motion_manager_t::get_instance()->add_module((motion_module_t*) kicking_t::get_instance());
     motion_manager_t::get_instance()->set_enable(true);
-
-    action_t::get_instance()->joint.set_enable_body(true, true);
-    action_t::get_instance()->start(9);
     if (m_debug) LOG_INFO << "Motion modules are ready";
 }
 
@@ -413,6 +410,8 @@ void robot_application_t::init_behavior() {
 
 void robot_application_t::start_main_loop() {
     if (m_debug) LOG_INFO << "=== Controller has started ===";
+    action_t::get_instance()->joint.set_enable_body(true, true);
+    action_t::get_instance()->start(9);
     while (is_running()) {
         if (m_debug) LOG_DEBUG << "ROBOT APPLICATION: === Iteration start ===";
         m_behavior->process();

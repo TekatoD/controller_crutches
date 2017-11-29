85c499e5c9547bc0be4fb5d45bce49b7f9073607
Arseniy Ivin <arssivka@yandex.ru>
2017-11-29 16:39:39 +0300

    Some changes

--- src/behavior/go_to_t.cpp
+++ src/behavior/go_to_t.cpp
@@ -5,6 +5,7 @@
 
 #include <math/angle_tools.h>
 #include <log/trivial_logger_t.h>
+#include <boost/math/constants/constants.hpp>
 #include "behavior/go_to_t.h"
 #include "motion/modules/walking_t.h"
 
@@ -28,13 +29,18 @@ void drwn::go_to_t::process(drwn::pose2d_t pos) {
     }
     m_done = true;
     float dist = std::hypot(pos.get_x(), pos.get_y());
-    float angle = degrees(std::atan2(pos.get_y(), pos.get_x())); // It used when robot goes with turning
+    float angle = std::atan2(pos.get_y(), pos.get_x()); // It used when robot goes with turning
     pose2d_t odo = walking_t::get_instance()->get_odo();
 
+    if (m_debug) {
+        LOG_DEBUG << "GO TO: dist = " << dist;
+        LOG_DEBUG << "GO TO: angle = " << degrees(angle);
+    }
+
     if (!walking_t::get_instance()->is_running() ||
-        walking_t::get_instance()->get_x_move_amplitude() != m_x ||
-        walking_t::get_instance()->get_x_move_amplitude() != m_y ||
-        walking_t::get_instance()->get_x_move_amplitude() != m_a) {
+            walking_t::get_instance()->get_x_move_amplitude() != m_x ||
+            walking_t::get_instance()->get_y_move_amplitude() != m_y ||
+            walking_t::get_instance()->get_a_move_amplitude() != m_a) {
         m_x = walking_t::get_instance()->get_x_move_amplitude();
         m_y = walking_t::get_instance()->get_y_move_amplitude();
         m_a = walking_t::get_instance()->get_a_move_amplitude();
@@ -42,24 +48,25 @@ void drwn::go_to_t::process(drwn::pose2d_t pos) {
 
     if (dist > m_distance_var) {
         float angle_diff = angle - odo.get_theta();
-        m_goal_max_speed = std::max(std::cos(angle_diff), 0.0f) * (dist < m_fit_distance ? m_fit_speed : m_max_speed);
-        float dir = (std::fabs(angle_diff) > m_angle_var ? 1.0f : -1.0f);
+        auto cangle_diff = std::cos(angle_diff);
+        m_goal_max_speed = (dist < m_fit_distance ? m_fit_speed : m_max_speed);
         // Reduce x and y and increase a when diff greater than allowable angle variance
-        m_x += dir * (m_x > 0 ? -m_step_accel : m_step_accel);
+        m_x += (cangle_diff > 0.0f ? m_step_accel : -m_step_accel);
         m_y += 0.0f;
-        m_a += dir * (angle_diff > 0.0 ? m_turn_accel : -m_turn_accel);
+        m_a += (angle_diff > 0.0f ? m_turn_accel : -m_turn_accel);
+        if (std::fabs(angle_diff) > radians(m_angle_var)) {
+            m_a *= std::fabs(angle_diff) / radians(m_angle_var);
+        }
         // Reset speed when it very small
         float x_abs = std::fabs(m_x);
-        if (x_abs < m_step_accel * 0.95f) {
+        if (x_abs < m_step_accel * 0.9f) {
             m_x = 0.0;
         } else if (x_abs > m_goal_max_speed) {
             m_x = std::copysign(m_goal_max_speed, m_x);
         }
 
         float a_abs = std::fabs(m_a);
-        if (a_abs < m_step_accel * 0.95f) {
-            m_a = 0.0;
-        } else if (a_abs > m_max_turn) {
+        if (a_abs > m_max_turn) {
             m_a = std::copysign(m_max_turn, m_a);
         }
 
@@ -84,15 +91,21 @@ void drwn::go_to_t::process(drwn::pose2d_t pos) {
         }
     }
 
-
     if (!m_done) {
-        m_update_rate.update();
+        if (m_debug) {
+            LOG_DEBUG << "GO TO: x_amplitude = " << m_x;
+            LOG_DEBUG << "GO TO: y_amplitude = " << m_y;
+            LOG_DEBUG << "GO TO: a_amplitude = " << m_a;
+        }
         walking_t::get_instance()->joint.set_enable_body_without_head(true, true);
         walking_t::get_instance()->set_x_move_amplitude(m_x);
         walking_t::get_instance()->set_y_move_amplitude(m_y);
         walking_t::get_instance()->set_a_move_amplitude(m_a);
         walking_t::get_instance()->start();
     } else {
+        if (m_debug) {
+            LOG_DEBUG << "GO TO: Processing done";
+        }
         walking_t::get_instance()->stop();
     }
 }
